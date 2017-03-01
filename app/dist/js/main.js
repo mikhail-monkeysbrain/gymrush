a124870b86120eeedd253e871c44488dba796e47
Mikail Stolpovskikh <monkeysbrain@yandex.ru>
2017-03-01 18:35:21 +0500

    fix parallax

--- app/dist/js/main.js
+++ app/dist/js/main.js
@@ -18,10 +18,18 @@ $( function() {
 $(function(){
 	$(document).ready(function(){
 		$window = $(window);
-		$('section[data-type="background"]').each(function(){
+		$('.depth-1[data-type="background"]').each(function(){
 			var $bgobj = $(this);
 			$(window).scroll(function() {         
 				var yPos = -($window.scrollTop() / ($bgobj.data('speed')/20)); 
+				var coords = '50% '+ yPos + 'px';
+				$bgobj.css({ backgroundPosition: coords });
+			}); 
+ 		});	
+		$('.depth-2[data-type="background"]').each(function(){
+			var $bgobj = $(this);
+			$(window).scroll(function() {         
+				var yPos = -($window.scrollTop() / ($bgobj.data('speed')/5)); 
 				var coords = '50% '+ yPos + 'px';
 				$bgobj.css({ backgroundPosition: coords });
 			}); 
